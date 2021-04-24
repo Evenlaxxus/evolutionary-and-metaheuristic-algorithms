da508633d336989258860af84b1d9ff9c0e77631
Evenlaxxus <r.ewiak1404@wp.pl>
2021-04-24 19:58:30 +0200

    Fix steepest local search

--- project2/steepest_local_search.py
+++ project2/steepest_local_search.py
@@ -1,41 +1,111 @@
 import itertools
+import sys
+
 import numpy as np
 import utils
 
 
-def load(filename):
-    return np.genfromtxt(filename, skip_header=6, skip_footer=1, dtype='int64')[:, 1:]
+def findOutsideSwap(distanceMatrix, path, not_path, i, j):
+    v1 = path[i - 1]
+    v2 = path[i]
+    v3 = path[i + 1]
+    edge1 = distanceMatrix[v1, v2]
+    edge2 = distanceMatrix[v2, v3]
+
+    newV = not_path[j]
+
+    newEdge1 = distanceMatrix[v1, newV]
+    newEdge2 = distanceMatrix[newV, v3]
+
+    currentLength = edge1 + edge2
+    newLength = newEdge1 + newEdge2
+
+    delta = newLength - currentLength
+    return (i, j), delta
 
 
-def steepest(distances, type):
-    number_of_all = distances.shape[0]
+def findEdgeSwap(distanceMatrix, path, i, j):
+    v1 = path[i]
+    v2 = path[i + 1]
+    edge1 = distanceMatrix[v1, v2]
+
+    v3 = path[j]
+    v4 = path[j + 1]
+    edge2 = distanceMatrix[v3, v4]
+
+    newEdge1 = distanceMatrix[v1, v3]
+    newEdge2 = distanceMatrix[v2, v4]
+
+    currentLength = edge1 + edge2
+    newLength = newEdge1 + newEdge2
+
+    delta = newLength - currentLength
+    return (i + 1, j), delta
+
+
+def bestOutsideSwap(distanceMatrix, path, not_path, i):
+    bestMove = tuple()
+    bestDelta = sys.maxsize
+    for j in range(len(not_path)):
+        move, delta = findOutsideSwap(distanceMatrix, path, not_path, i, j)
+        if delta < bestDelta:
+            bestMove = move
+            bestDelta = delta
+    return bestMove, bestDelta
+
+
+def bestEdgeSwap(distanceMatrix, path, i):
+    bestMove = tuple()
+    bestDelta = sys.maxsize
+    for j in range(len(path) - 1):
+        if j != i and j != i + 1 and j != i - 1:
+            move, delta = findEdgeSwap(distanceMatrix, path, i, j)
+            if delta < bestDelta:
+                bestMove = move
+                bestDelta = delta
+    return bestMove, bestDelta
+
+
+def getBestMove(distanceMatrix, path, not_path):
+    bestMove = tuple()
+    moveType = None
+    bestMoveDelta = sys.maxsize
+    for i in range(len(path) - 1):
+        move, delta = bestOutsideSwap(distanceMatrix, path, not_path, i)
+        if delta < bestMoveDelta:
+            bestMove = move
+            bestMoveDelta = delta
+            moveType = "outside"
+
+        move, delta = bestEdgeSwap(distanceMatrix, path, i)
+        if delta < bestMoveDelta:
+            bestMove = move
+            bestMoveDelta = delta
+            moveType = "edge"
+    return moveType, bestMove, bestMoveDelta
+
+
+def steepest(distanceMatrix, type):
+    number_of_all = distanceMatrix.shape[0]
     number_of_half = int(np.ceil(number_of_all / 2))
     path, not_path = utils.make_random_path(number_of_half, number_of_all)
-    actions = utils.swap_actions(number_of_half, type)
-    result_of_itertools_product = itertools.product(np.arange(path.shape[0]), np.arange(not_path.shape[0]))
-    exchange_actions = np.array(list(result_of_itertools_product), 'int')
 
     while True:
-        swap_delta = [utils.calculate_swap_delta(one_swap, path, distances, type) for one_swap in actions]
-        exchange_delta = [utils.calculate_exchange_delta(one_exchange, path, not_path, distances) for one_exchange in
-                          exchange_actions]
-        id_max_swap = int(np.argmax(swap_delta))
-        id_exchange_max = int(np.argmax(exchange_delta))
-        max_swap = swap_delta[id_max_swap]
-        max_exchange = exchange_delta[id_exchange_max]
-        if max_swap <= 0 and max_exchange <= 0:
+        moveType, move, moveDelta = getBestMove(distanceMatrix, path, not_path)
+        if moveDelta >= 0:
             break
-        if max_swap > max_exchange:
-            utils.swap(path, actions[id_max_swap], type)
-        else:
-            utils.exchange_vertices(path, not_path, exchange_actions[id_exchange_max])
+        v1, v2 = move
+        if moveType == "edge":
+            path[v1:v2 + 1] = np.flip(path[v1:v2 + 1])
+        elif moveType == "outside":
+            path[v1], not_path[v2] = not_path[v2], path[v1]
     return np.append(path, path[0])
 
 
 def main():
     kroA100Data = utils.readTspFile("../data/kroA100.tsp")
     utils.localSearchTester(steepest, kroA100Data, "edges")
-    utils.localSearchTester(steepest, kroA100Data, "vertices")
+    # utils.localSearchTester(steepest, kroA100Data, "vertices")
 
 
 if __name__ == "__main__":
