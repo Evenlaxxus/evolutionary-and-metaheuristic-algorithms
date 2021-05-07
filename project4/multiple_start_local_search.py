59e3fb1bc5f9d025090bd526b862f6c53bb8bee8
Evenlaxxus <r.ewiak1404@wp.pl>
2021-05-07 18:37:42 +0200

    Adds multiple start local search

---
+++ project4/multiple_start_local_search.py
@@ -0,0 +1,55 @@
+import time
+
+import project3.candidate_local_search as steepest
+import utils
+import numpy as np
+
+
+def testMultipleStartLocalSearch(testerRuns, algorithmRuns):
+    data = utils.readTspFile("../data/kroA200.tsp")
+
+    paths = []
+    times = []
+    for _ in range(testerRuns):
+        start = time.time()
+        paths.append(multipleStartLocalSearch(data, algorithmRuns))
+        end = time.time()
+        times.append(end - start)
+
+    minRun = min(paths, key=lambda t: t[0])
+    maxRun = max(paths, key=lambda t: t[0])
+
+    distances = [p[0] for p in paths]
+    averageDistance = sum(distances) / len(distances)
+
+    minTime = min(times)
+    maxTime = max(times)
+    averageTime = sum(times) / len(times)
+
+    print("minimalny dystans: " + str(minRun[0]), "średni dystans: " + str(averageDistance),
+          "maksymalny dystans: " + str(maxRun[0]))
+    print("minimalny czas: " + str(minTime), "maksymalny czas: " + str(maxTime),
+          "średni czas: " + str(averageTime))
+
+    utils.drawGraph(data, minRun[1])
+
+
+def multipleStartLocalSearch(data, n):
+    paths = []
+    distanceMatrix = utils.makeDistanceMatrix(data)
+    distances = np.array(utils.makeDistanceMatrix(data))
+
+    for _ in range(n):
+        paths.append(steepest.steepest(distances, ""))
+
+    distances = []
+    for path in paths:
+        distances.append(utils.calculatePathDistance(path, distanceMatrix))
+
+    minDistance = min(distances)
+    minPath = paths[distances.index(minDistance)]
+    return minDistance, minPath
+
+
+if __name__ == '__main__':
+    testMultipleStartLocalSearch(10, 100)
