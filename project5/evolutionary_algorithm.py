9aa36e070edf151962f5da05735a8800361b6922
thieminho <miloszo28@gmail.com>
2021-05-22 13:06:16 +0200

    add recombination

--- project5/evolutionary_algorithm.py
+++ project5/evolutionary_algorithm.py
@@ -1,21 +1,27 @@
+import math
 import time
 import utils
+import random
+import numpy as np
 from project3.candidate_local_search import steepest
 
 
 def evolutionaryTravelingSalesman(distanceMatrix, timeout):
-    population = []
-
+    city_list = [x for x in range(0, 200)]
+    population = make_populations(20, city_list)
+    print(population)
     timeout_start = time.time()
     while time.time() < timeout_start + timeout:
         # two random parents
-        parents = []
+        parents = [[],[]]
+        parents[0] = population[random.randint(0, len(population) - 1)]
+        parents[1] = population[random.randint(0, len(population) - 1)]
 
         # construct descendant
         descendant = recombination(parents)
 
         # local search
-        descendant = steepest(distanceMatrix, descendant)
+        descendant = steepest(np.array(distanceMatrix), descendant)
         descendantDistance = utils.calculatePathDistance(descendant, distanceMatrix)
 
         # steady state
@@ -34,4 +40,42 @@ def evolutionaryTravelingSalesman(distanceMatrix, timeout):
 
 
 def recombination(parents):
-    return []
+    parent_1 = parents[0]
+    parent_2 = parents[1]
+    common = list(set(parent_1).intersection(parent_2))
+    combined = []
+    for i in range(len(parent_1)):
+        if parent_1[i] in common:
+            combined.append(parent_1[i])
+        else:
+            choice = random.randint(0, 1)
+            if choice == 0 and parent_2[i] not in common:
+                combined.append(parent_2[i])
+            else:
+                combined.append(parent_1[i])
+    return combined
+
+
+
+def createRoute(city_list):
+    route = random.sample(city_list, math.ceil(len(city_list)/2))
+    return route
+
+def make_populations(size, city_list):
+    count = 0
+    population = []
+    while count < size:
+        new = createRoute(city_list)
+        if new not in population:
+            population.append(new)
+            count += 1
+    return population
+
+
+def main():
+    data = utils.readTspFile("../data/kroA200.tsp")
+    distanceMatrix = utils.makeDistanceMatrix(data)
+    evolutionaryTravelingSalesman(distanceMatrix, 15)
+
+if __name__ == '__main__':
+    main()
